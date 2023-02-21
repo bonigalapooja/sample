6ea37f6da5c6db424ef6270ad89e6d341d879f9b
bonigalapooja <poojitha.b@tuitionworld.co.in>
2023-02-21 17:52:37 +0530

    first

---
+++ client/src/Mode.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Mode() {
+  return (
+    <>
+        <div className="contain">
+          <form>
+            <input type="text" value={result}/>
+          </form>
+          <div className="keypad">
+            <button>claer</button>
+            <button>claer</button>
+            <button>claer</button>
+            <button>claer</button>
+          
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Mode
---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+import Mode from './Mode';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Mode/>
+  </React.StrictMode>
+);
+
+
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
